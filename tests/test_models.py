beaaf5fbaaf5014522873cbdd00f5fccb2958cc0
t.latour <t.latour@fz-juelich.de>
2024-12-09 15:24:44 +0100

    try resolveing the fmu path

--- tests/test_models.py
+++ tests/test_models.py
@@ -41,7 +41,7 @@ def test_simulate(dummy_model):
 
 
 class TestFmuModel:
-    test_fmu_path = Path(__file__).absolute().parent / "test_data/growth.fmu"
+    test_fmu_path = Path(__file__).absolute().parent.parent / "tests/test_data/growth.fmu"
 
     def test_retrieve_variables(self):
         fmu_model = FmuModel(self.test_fmu_path)
