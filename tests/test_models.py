ca7c72658b89446347a49bcd0ff496b1d6e6cba2
t.latour <t.latour@fz-juelich.de>
2024-12-13 15:45:24 +0100

    list files in test_data

--- tests/test_models.py
+++ tests/test_models.py
@@ -42,9 +42,7 @@ def test_simulate(dummy_model):
 
 
 class TestFmuModel:
-    test_fmu_path = (
-        Path(__file__).absolute().parent.parent / "tests/test_data/growth.fmu"
-    )
+    test_fmu_path = "tests/test_data/growth.fmu"
 
     def test_retrieve_variables(self):
         fmu_model = FmuModel(self.test_fmu_path)
