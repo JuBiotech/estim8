053893e4bf0ab284b325365665c3de5ad76a8840
t.latour <t.latour@fz-juelich.de>
2024-12-13 15:54:43 +0100

    fix spelling of testmodel Growth

--- tests/test_models.py
+++ tests/test_models.py
@@ -42,7 +42,7 @@ def test_simulate(dummy_model):
 
 
 class TestFmuModel:
-    test_fmu_path = "tests/test_data/growth.fmu"
+    test_fmu_path = "tests/test_data/Growth.fmu"
 
     def test_retrieve_variables(self):
         fmu_model = FmuModel(self.test_fmu_path)
